22722b0a52c3e298cbb98e562cf21b6d695341f1
nizihad824 <mdnurulislam97@gmail.com>
2022-10-14 01:06:29 +0200

    order collection apiu done

--- index.js
+++ index.js
@@ -21,6 +21,7 @@ async function run(){
     try{
         await client.connect();
         const serviceCollection = client.db('geniusCar').collection('service');
+        const orderCollection =client.db('geniusCar').collection('order')
 
         app.get('/service',async(req,res)=>{
             const query ={};
@@ -47,6 +48,21 @@ async function run(){
             const result =await serviceCollection.deleteOne(query)
             res.send(result)
 
+        })
+        //order collection API
+        app.get('/order',async(req,res) =>{
+            const email =req.query.email;
+            const query ={email:email};
+            const cursor =orderCollection.find(query);
+            const orders =await cursor.toArray();
+            res.send(orders);
+            console.log(orders)
+
+        })
+        app.post('/order',async (req,res) =>{
+            const order =req.body;
+            const result =await orderCollection.insertOne(order)
+            res.send(result)
         })
     }
     finally{
